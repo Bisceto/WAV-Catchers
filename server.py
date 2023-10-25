0981c19521befd4a7875f6afdd7629cbbd5c56fa
Wesley Teo <wesley.tyx@gmail.com>
2023-10-25 15:04:17 +0800

    Implemented function to save audio recordings when received from microphone in the form of WAV files.

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
 import paho.mqtt.client as mqtt
-
-from time import sleep
+import time
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code: " + str(rc))
+	client.subscribe("sensors/#")
+	client.subscribe("telegram/#")
+
+def on_message(client, userdata, message):
+	topic = message.topic
+	data = message.payload
+	timestamp = time.time()
+
+	if topic.startswith("sensors/audio"):
+		save_audio_file(topic, data)
 	
+	print("Received message")
+
+def save_audio_file(topic, data, timestamp):
+	filename = topic.split("/")[-1] + str(timestamp)
+	filepath = "./audio/" + filename
+	with open(filepath, "wb") as audio_file:
+		audio_file.write(data)
 
 client = mqtt.Client()
 client.on_connect = on_connect
