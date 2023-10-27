ca951e015d65d1b3d4aa87d733e17a4f544f1d9b
Hidejy <menjiying@gmail.com>
2023-10-27 16:31:28 +0800

    add pir_sensor,updated esp32 cam

--- mqtt_camera.py
+++ mqtt_camera.py
@@ -2,16 +2,16 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: " + str(rc))
-    client.subscribe("proximity")
+    client.subscribe("motion")
 
 def on_message(client, userdata, message):
-    if message.topic == "proximity":
+    if message.topic == "motion":
         proximity_state = message.payload.decode()
         print(f"Proximity Sensor State: {proximity_state}")
 
-        if proximity_state == "near":
+        if proximity_state == "object detected":
             client.publish("classification", "Take photo")
-        elif proximity_state == "far":
+        elif proximity_state == "no object":
             client.publish("classification", "Don't take photo")
 
 client = mqtt.Client()
