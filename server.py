3cf62d2a78acc69d0c96d1f788180deffd4b9f0b
aaron <ashwinaaron@gmail.com>
2023-11-06 22:38:15 +0800

    on hold

--- server.py
+++ server.py
@@ -9,6 +9,32 @@ import numpy as np
 
 client = mqtt.Client()
 
+def handle_motion():
+    url = 'http://172.20.10.6/cam-hi.jpg'
+    labels = []
+    for i in range(10): #10 frames
+        img_resp=urllib.request.urlopen(url)
+        imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
+        im = cv2.imdecode(imgnp,-1)
+        bbox, label, conf = cv.detect_common_objects(im, model="yolov3")
+        output_image = draw_bbox(im, bbox, label, conf)
+
+        for item in label:
+            if item in labels:
+                pass
+            else:
+                labels.append(item)
+
+        cv2.imshow("At your door", output_image)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.destroyWindow("At your door")
+            break
+    print(labels)
+    if "person" in labels:
+        client.publish("detection/camera", "Person detected")
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: " + str(rc))
     client.subscribe("detection/sensor")
@@ -18,29 +44,7 @@ def on_message(client, userdata, message):
     print("Received message " + str(message.payload))
     temp = int(message.payload)
     if temp == 1:
-        url = 'http://172.20.10.6/cam-hi.jpg'
-        labels = []
-        for i in range(10): #10 frames
-            img_resp=urllib.request.urlopen(url)
-            imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-            im = cv2.imdecode(imgnp,-1)
-            bbox, label, conf = cv.detect_common_objects(im, model="yolov3")
-            output_image = draw_bbox(im, bbox, label, conf)
-
-            for item in label:
-                if item in labels:
-                    pass
-                else:
-                    labels.append(item)
-
-            cv2.imshow("At your door", output_image)
-
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                cv2.destroyWindow("At your door")
-                break
-        print(labels)
-        if "person" in labels:
-            client.publish("detection/camera", "Person detected")
+        handle_motion()
 
 client.on_connect = on_connect
 client.on_message = on_message
